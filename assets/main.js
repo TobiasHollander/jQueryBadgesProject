51fcdc681679cbceafa1d78e93c124eb9f6bf38b
Tobias Hollander <tobiashollander@Tobiass-MacBook-Pro.local>
2018-04-16 11:08:17 -0500

    first commit

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,15 @@
 $(function() {
 
-  // your code will go here
+$.ajax({
+  url: "https://www.codeschool.com/users/tobiashollander.json",
+  dataType: "jsonp",
+  success: function(response){
+    $.each(response.courses.completed, function(i){
+      $("#badges").append("<div class='course'><h3>"+response.courses.completed[i].title+"</h3><img src='"+response.courses.completed[i].badge+"'/><a class='btn btn-primary' href="+response.courses.completed[i].url+" target='_blank'>See Courses</a></div>");
+      console.log(response);
+      console.log("current index " + i);
+    });
+  }
+});
 
 });
